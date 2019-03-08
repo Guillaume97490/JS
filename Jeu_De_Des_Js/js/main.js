ebd47125abcefd0070560e92c62b211c2594da44
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-03-08 06:46:52 +0400

    calculatrice_+ecran

--- Jeu_De_Des_Js/js/main.js
+++ Jeu_De_Des_Js/js/main.js
@@ -1,57 +1,62 @@
+var scoreDefJoueur1 = 0;
+var scoreDefJoueur2 = 0;
+
+var currentScore1 = 0;
+var currentScore2 = 0;
+
+var main=1
+
+/* function choixPlayer(){
+    if 
+} */
 
 
-var scoreDefJoueurA = 0;
-var scoreDefJoueurB = 0;
-
-var currentScoreA = 0;
-var currentScoreB = 0;
-
-
-function jeuDes(){
-var des6 = [1, 2, 3, 4, 5, 6];
-var desNouvLancer = des6[Math.floor(Math.random() * des6.length)];
-
-document.getElementById("diceFace").src= "images/dice-" + desNouvLancer + ".png"
-
-if(desNouvLancer === 1){
-    currentScoreA = 0;
-    //alert("Le des a fait : " + desNouvLancer +"\nScore du JoueurA : " + currentScoreA)
-
-} else{
-    
-    currentScoreA += desNouvLancer;
-    //alert("Le des a fait : " + desNouvLancer +"\nScore du JoueurA : " + currentScoreA)
-
+function newgame() {
+    scoreDefJoueur1 = 0;
+    scoreDefJoueur2 = 0;
+    currentScore1 = 0;
+    currentScore2 = 0;
+    document.getElementById("ScorePlayer1").innerHTML = scoreDefJoueur1;
+    document.getElementById("ScoreCurrent1").innerHTML = currentScore1;
 }
 
-}
-
-function newgame(){
-scoreDefJoueurA = 0;
-scoreDefJoueurB = 0;
-
-currentScoreA = 0;
-currentScoreB = 0;
-document.getElementById("ScorePlayer1").innerHTML = scoreDefJoueurA;
-document.getElementById("ScoreCurrent1").innerHTML = currentScoreA;
-
-
-}
-
-function relance(){
+function relance() {
     jeuDes()
-    document.getElementById("ScoreCurrent1").innerHTML = currentScoreA;
+    document.getElementById("ScoreCurrent1").innerHTML = currentScore1;
 }
 
-function garderA(){
-    scoreDefJoueurA += currentScoreA;
-    //alert("le joueur A garde " + scoreDefJoueurA + "points") 
-    currentScoreA = 0;
-    document.getElementById("ScorePlayer1").innerHTML = scoreDefJoueurA;
-    document.getElementById("ScoreCurrent1").innerHTML = currentScoreA;
-    if(scoreDefJoueurA >= 100){
+function garder() {
+    scoreDefJoueur1 += currentScore1;
+    //alert("le joueur  1 garde " + scoreDefJoueur1 + "points") 
+    currentScore1 = 0;
+    document.getElementById("ScorePlayer1").innerHTML = scoreDefJoueur1;
+    document.getElementById("ScoreCurrent1").innerHTML = currentScore1;
+    if (scoreDefJoueur1 >= 100) {
         alert("Player 1 à gagner : ")
+    }
+}
+
+function jeuDes() {
+    var des6 = [1, 2, 3, 4, 5, 6];
+    var desNouvLancer = des6[Math.floor(Math.random() * des6.length)];
+
+    document.getElementById("diceFace").src = "images/dice-" + desNouvLancer + ".png"
+
+    if (desNouvLancer === 1) {
+        currentScore1 = 0;
+
+        //alert("Le des a fait : " + desNouvLancer +"\nScore du Joueur 1 : " + currentScore1)
+
+    } else {
+
+        currentScore1 += desNouvLancer;
+        //alert("Le des a fait : " + desNouvLancer +"\nScore du Joueur 1 : " + currentScore1)
+
     }
 
 }
+
+
+
+
 
